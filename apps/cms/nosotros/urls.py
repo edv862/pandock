6afd74ddcaa3d1f794ced837f91456cecfd041b9
edgar <evalderrama862@gmail.com>
2018-07-20 11:50:47 -0400

    url fix for production

--- apps/cms/nosotros/urls.py
+++ apps/cms/nosotros/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import NosotrosView
 
@@ -6,3 +8,5 @@ app_name = 'nosotros'
 urlpatterns = [
     url(r'^$', NosotrosView.as_view(), name='view'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pandock/urls.py
+++ pandock/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^', include('apps.cms.home.urls', namespace='home')),
@@ -23,3 +26,5 @@ urlpatterns = [
     url(r'^sedes/', include('apps.cms.donde_estamos.urls', namespace='donde_estamos')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
