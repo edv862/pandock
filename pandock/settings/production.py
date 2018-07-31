839526044ca2c2a27a9e15edf5f2bbeb381500c7
edgar <evalderrama862@gmail.com>
2018-07-30 20:52:06 -0400

    Background pdf

--- pandock/settings/production.py
+++ pandock/settings/production.py
@@ -24,7 +24,7 @@ LANGUAGE_CODE = 'es-ve'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
