76bf6ee79dd37e003be97acdcd3eadbf0a2c088d
edv862 <evalderrama862@gmail.com>
2018-07-31 01:09:33 +0000

    Production settings

--- pandock/settings/production.py
+++ pandock/settings/production.py
@@ -15,19 +15,22 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [	
     'http://jhonns862.pythonanywhere.com/',
+    'jhonns862.pythonanywhere.com/',
+    'jhonns862.pythonanywhere.com',
 ]
 
 LANGUAGE_CODE = 'es-ve'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
