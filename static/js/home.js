b39476ceaf83ac3720a52cf44a41e9e5fd9a7251
edgar <evalderrama862@gmail.com>
2018-08-14 13:22:19 -0400

    Pausa slider noticias

--- static/js/home.js
+++ static/js/home.js
@@ -1,18 +1,3 @@
-$(function()
-{
-    var ticker = function()
-    {
-        setTimeout(function(){
-            $('.noticias li:first').animate( {'marginTop': '-275px'}, 2000, function()
-            {
-                $(this).detach().appendTo('.noticias').removeAttr('style'); 
-            });
-            ticker();
-        }, 10000);
-    };
-    ticker();
-});
-
 $(document).ready(function() {
     $('.carousel-item, .home-slider-control-next, .home-slider-control-prev, .carousel-custom-prev-icon, .carousel-custom-next-icon').hover(
         function(){
