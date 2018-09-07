c65bd11cdaa36c0d8fad2f43923e97d64233f86c
edv862 <evalderrama862@gmail.com>
2018-09-07 10:17:06 +0000

    Fixes server

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,27 +1,12 @@
-$(function()
-{
-    var ticker = function()
-    {
-        setTimeout(function(){
-            $('.noticias li:first').animate( {'marginTop': '-275px'}, 2000, function()
-            {
-                $(this).detach().appendTo('.noticias').removeAttr('style'); 
-            });
-            ticker();
-        }, 10000);
-    };
-    ticker();
-});
-
 $(document).ready(function() {
-    $('.carousel-item, .carousel-control-prev, .carousel-control-next, .carousel-control-prev-icon, .carousel-control-next-icon').hover(
+    $('.carousel-item, .home-slider-control-next, .home-slider-control-prev, .carousel-custom-prev-icon, .carousel-custom-next-icon').hover(
         function(){
-            $('.carousel-control-prev-icon').css('display', 'block');
-            $('.carousel-control-next-icon').css('display', 'block');
+            $('.carousel-custom-prev-icon').css('display', 'block');
+            $('.carousel-custom-next-icon').css('display', 'block');
         },
         function(){
-            $('.carousel-control-prev-icon').css('display', 'none');
-            $('.carousel-control-next-icon').css('display', 'none');
+            $('.carousel-custom-prev-icon').css('display', 'none');
+            $('.carousel-custom-next-icon').css('display', 'none');
         }
     );
 });
